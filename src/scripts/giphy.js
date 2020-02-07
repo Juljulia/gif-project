2b36d28797c61ddf46d09c56115f03446e8d2239
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-02-07 08:14:29 +0100

    Update giphy.js

--- src/scripts/giphy.js
+++ src/scripts/giphy.js
@@ -2,25 +2,19 @@
 //the API calls against Giphy's API and return data.
 
 const input = document.querySelector("input");
-//input value is what we type in searchfield
+const gallery = document.querySelector(".gallery");
 
 const giphySearch = function() {
-fetch(`https://api.giphy.com/v1/gifs/search?api_key=YgmD4R9tFKiWIIe0UinPpSOpYhxH8nyG&q=${input.value}`)
-    .then(response => response.json())
-    .then(response => {
-        const images = response.data; 
-        images.forEach(image => {
-            const image = new Image();
-              image.src = response.data[0].images.downsized.url;
-              // Add the image to the body.
-              document.body.appendChild(image);
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=YgmD4R9tFKiWIIe0UinPpSOpYhxH8nyG&q=${input.value}`)
+        .then(response => response.json())
+        .then(json => {
+            const images = json.data; 
+            images.forEach(image => {
+                const url = image.images.downsized.url;
+                const galleryItem = `<a class="gallery__item"><img class="gallery__item__image" src="${url}"></a>`;
+                gallery.innerHTML += galleryItem;
+            });
         });
-            
-    
-              
-            
-    });
-
-}
+};
 
 export default giphySearch;
